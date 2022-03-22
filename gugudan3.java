6d911caa762f1d541796aa6d3488652705323586
tjdghrntgoq <101791473+tjdghrntgoq@users.noreply.github.com>
2022-03-22 14:09:49 +0900

    Add files via upload

---
+++ gugudan3.java
@@ -0,0 +1,16 @@
+package dya0322;
+
+public class gugudan3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =3;
+		int b=1;
+		for (int i=0; i<8; i++)
+		{
+			b=b+1;
+			System.out.println(a+"*"+b+"="+a*b);
+		}
+	}
+
+}
