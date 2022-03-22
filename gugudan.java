d49c012a73093f50a1f40fca9cd3ed11d75d7f07
tjdghrntgoq <101791473+tjdghrntgoq@users.noreply.github.com>
2022-03-22 14:28:27 +0900

    Update gugudan.java

--- gugudan.java
+++ gugudan.java
@@ -10,7 +10,7 @@ public class gugudan {
 		System.out.println("±¸±¸´ÜÀ» ÀÔ·ÂÇÏ½Ã¿ä");
 		int a =sc.nextInt();
 		int b=1;
-		for(int i=0; i<8; i++)
+		for(int i=0; i<9; i++)
 		{
 			System.out.println(a+"*"+b+"="+a*b);
 			b=b+1;
